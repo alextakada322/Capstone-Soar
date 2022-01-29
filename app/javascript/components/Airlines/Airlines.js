cbbe4a68fdae8d731986175fb2a4258bdf6601fb
AlexTakada <alex_takada@hotmail.com>
2022-01-29 02:25:50 -0800

    styled Airline cards and grid

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,32 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import Airline from './Airline'
+import styled from 'styled-components'
+
+const Home = styled.div`
+    text-align: center;
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+`
+const Header = styled.div`
+    padding: 100px 100px 10px 100px;
+
+    h1 {
+        font-size: 42px;
+    }
+`
+const Subheader = styled.div`
+    font-weight: 300;
+    font-size: 26px;
+`
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    width: 100%;
+    padding: 20px;
+`
 
 const Airlines = () => {
     const [airlines, setAirlines] = useState([]);
@@ -26,15 +52,15 @@ const Airlines = () => {
 
 
     return (
-    <div className="home">
-        <div className="header">
+    <Home>
+        <Header>
             <h1>Soar</h1>
-            <div className="subheader">unbiased airline review.</div>
-        </div>
-        <div className="grid">
+            <Subheader>unbiased airline review.</Subheader>
+        </Header>
+        <Grid>
             {grid}
-        </div>
-    </div>
+        </Grid>
+    </Home>
     )
 }
 
