aa09d7f5506d3dde47e5905128a92925bf315624
AlexTakada <alex_takada@hotmail.com>
2022-01-29 03:57:19 -0800

    added Review Form with styling and updated Header

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, {useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+import Header from './Header'
+import ReviewForm from './ReviewForm'
+import styled from 'styled-components'
 
-const Airline = () => {
-    return <div>This is the Airlines#show view for our app.</div>
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    
+`
+const Column = styled.div`
+    background: #fff;
+    height: 100vh;
+    overflow: scroll;
+    
+    &:last-child {
+        background: #000;
+    }
+`
+const Main = styled.div`
+    padding-left: 50px;
+`
+
+
+const Airline = (props) => {
+    const [airline, setAirline] = useState({})
+    const [review, setReview] = useState({})
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(()=>{
+        const slug = props.match.params.slug
+        const url = `/api/v1/airlines/${slug}`
+        // api/v1/airlines/united-airlines
+        // how to get slug value construct url for apiendpoint
+        // airlines/united-airlines
+        axios.get(url)
+        .then( resp => {
+            setAirline(resp.data)
+            setLoaded(true)
+            //boolean acts as switch to when we have our data to load in Header componennt
+        })
+        .catch( resp => console.log(resp))
+        }, [])
+
+        
+
+    return (
+        <Wrapper> 
+            { 
+                loaded &&
+            <Fragment>
+                <Column>
+                    <Main>
+                        <Header
+                    attributes={airline.data.attributes}
+                    reviews={airline.included}
+                    /> 
+                
+                    <div className="reviews"></div>
+                    </Main>
+                </Column>
+                <Column>
+                    <ReviewForm/>
+                </Column>
+            </Fragment>
+            }
+        </Wrapper>
+    )
 }
 
 export default Airline
